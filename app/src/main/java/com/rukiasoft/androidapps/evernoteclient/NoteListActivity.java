d28c93da51817d0fcb1d0cb369712354224e46c4
Raúl Feliz <raulfeliz@gmail.com>
2016-02-06 13:27:36 +0100

    feat: set up an EvernoteSession

--- app/src/main/java/com/rukiasoft/androidapps/evernoteclient/NoteListActivity.java
+++ app/src/main/java/com/rukiasoft/androidapps/evernoteclient/NoteListActivity.java
@@ -2,14 +2,20 @@ package com.rukiasoft.androidapps.evernoteclient;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class NoteListActivity extends AppCompatActivity {
+import com.evernote.client.android.EvernoteSession;
+import com.evernote.client.android.login.EvernoteLoginFragment;
+
+public class NoteListActivity extends AppCompatActivity implements EvernoteLoginFragment.ResultCallback{
+
+    private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
+    private EvernoteSession mEvernoteSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,17 @@ public class NoteListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                loginIntoEvernote();
             }
         });
+        mEvernoteSession = new EvernoteSession.Builder(this)
+                .setEvernoteService(EVERNOTE_SERVICE)
+                .setSupportAppLinkedNotebooks(false)
+                .build(BuildConfig.EVERNOTE_CONSUMER_KEY, BuildConfig.EVERNOTE_CONSUMER_SECRET)
+                .asSingleton();
+
+        // TODO: 6/2/16 remove this. only forces reauthorization (for testing)
+        mEvernoteSession.logOut();
     }
 
     @Override
@@ -48,5 +61,24 @@ public class NoteListActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loginIntoEvernote() {
+        if(!mEvernoteSession.isLoggedIn()) {
+            mEvernoteSession.authenticate(this);
+        } else {
+            // TODO: 6/2/16 logged, show notes
+        }
+    }
+
+    @Override
+    public void onLoginFinished(boolean successful) {
+        if (successful) {
+            Toast.makeText(getApplicationContext(), "yuhuuuu", Toast.LENGTH_LONG).show();
+        } else {
+            // Do not change view and show a message
+            Toast.makeText(getApplicationContext(), "Could not login. Try again.", Toast.LENGTH_LONG).show();
+
+        }
     }
 }
