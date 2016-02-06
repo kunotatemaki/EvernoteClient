875e8da132343460ff716a16bf02cecd4a00a7b3
Raúl Feliz <raulfeliz@gmail.com>
2016-02-06 11:37:38 +0100

    feat: firs commit
    Uploading boilerplate

---
+++ app/src/main/java/com/rukiasoft/androidapps/evernoteclient/NoteListActivityFragment.java
@@ -0,0 +1,22 @@
+package com.rukiasoft.androidapps.evernoteclient;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NoteListActivityFragment extends Fragment {
+
+    public NoteListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_list, container, false);
+    }
+}
